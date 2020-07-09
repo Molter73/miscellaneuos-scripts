976f03ad6621c9b346626250d890a60ee7b75d55
Mauro Moltrasio <mauro.ezequiel@wazuh.com>
2020-07-09 11:52:31 +0200

    Improve generate_dirs with nested directories

--- Python/generate_dirs.py
+++ Python/generate_dirs.py
@@ -4,15 +4,28 @@ import string
 
 NUM_DIRS = 10
 NUM_FILES = 10
+RECURSION_LEVELS = 10
 
 BASE_DIR = 'C:\\testdir'
 
-if __name__ == "__main__":
-    for i in range(NUM_DIRS):
-        CURRENT_DIR = os.path.join(BASE_DIR, f'dir{i}')
-        if not os.path.isdir(CURRENT_DIR):
-            os.mkdir(CURRENT_DIR)
-        for j in range(NUM_FILES):
-            CURRENT_FILE = os.path.join(CURRENT_DIR, f'file{j}')
-            with open(CURRENT_FILE, 'w') as f:
+
+def fill_directory(path: str, dirs=10, files=10):
+    for i in range(dirs):
+        current_dir = os.path.join(path, f'dir{i}')
+        if not os.path.isdir(current_dir):
+            os.mkdir(current_dir)
+        for j in range(files):
+            current_file = os.path.join(current_dir, f'file{j}')
+            with open(current_file, 'w') as f:
                 f.write(random.choice(string.printable) * 1024)
+
+
+if __name__ == "__main__":
+    current_dir = BASE_DIR
+    fill_directory(current_dir, NUM_DIRS, NUM_FILES)
+
+    for i in range(RECURSION_LEVELS):
+        current_dir = os.path.join(current_dir, f'level{i}')
+        if not os.path.isdir(current_dir):
+            os.mkdir(current_dir)
+        fill_directory(current_dir, NUM_DIRS, NUM_FILES)
