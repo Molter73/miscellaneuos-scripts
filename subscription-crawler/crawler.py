9804995d01e7d4fbef63456e0e7f3fcbb9f287ef
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-02-24 12:07:51 +0100

    Crawl content sets asynchronously

--- subscription-crawler/crawler.py
+++ subscription-crawler/crawler.py
@@ -1,62 +1,81 @@
 import os
-import requests
 import json
+import requests
+import aiohttp
+import asyncio
 
 
-def get_refresh_token(offline_token: str) -> dict:
-    # -d client_id=rhsm-api -d refresh_token=$offline_toke
-    url = 'https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token'
-    data = {
-        'grant_type': 'refresh_token',
-        'client_id': 'rhsm-api',
-        'refresh_token': f'{offline_token}'
-    }
-    headers = {'accept': 'application/json'}
+class Crawler:
+    def __init__(self, offline_token: str):
+        self.token = self.get_refresh_token(offline_token)['access_token']
+        self.api_url = 'https://api.access.redhat.com/management/v1'
 
-    r = requests.post(url, data=data, headers=headers)
-
-    r.raise_for_status()
-
-    return r.json()
-
-
-def get_subscriptions(token: str) -> list:
-    limit = 50
-    offset = 0
-    count = 50
-    result = []
-
-    url = 'https://api.access.redhat.com/management/v1/subscriptions'
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'accept': 'application/json'
-    }
-
-    while count >= limit:
-        params = {
-            'limit': limit,
-            'offset': offset
+        self.headers = {
+            'Authorization': f'Bearer {self.token}',
+            'accept': 'application/json'
         }
-        r = requests.get(url, headers=headers, params=params)
+
+    def query_url(self, endpoint: str):
+        return f'{self.api_url}{endpoint}'
+
+    def get_refresh_token(self, offline_token: str) -> dict:
+        url = 'https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token'
+        data = {
+            'grant_type': 'refresh_token',
+            'client_id': 'rhsm-api',
+            'refresh_token': f'{offline_token}'
+        }
+
+        r = requests.post(url, data=data, headers={
+                          'accept': 'application/json'})
         r.raise_for_status()
 
-        response = r.json()
+        return r.json()
 
-        count = response['pagination']['count']
-        offset += count
-        yield response['body']
+    def get_subscriptions(self) -> list:
+        limit = 50
+        offset = 0
+        count = 50
+
+        while count >= limit:
+            params = {
+                'limit': limit,
+                'offset': offset
+            }
+            r = requests.get(self.query_url('/subscriptions'),
+                             headers=self.headers, params=params)
+            r.raise_for_status()
+
+            response = r.json()
+
+            count = response['pagination']['count']
+            offset += count
+            yield response['body']
+
+    async def get_content_sets(self, subscriptions: list):
+        for subscription in subscriptions:
+            subscription_number = subscription['subscriptionNumber']
+            url = self.query_url(
+                f'/subscriptions/{subscription_number}/contentSets')
+            print(f'Querying {url}')
+            async with self.session.get(url, headers=self.headers) as resp:
+                # await resp.json()
+                print(resp.status)
+
+
+    async def crawl(self):
+        async with aiohttp.ClientSession() as self.session:
+            tasks = []
+            for subscriptions in self.get_subscriptions():
+                tasks.append(asyncio.ensure_future(self.get_content_sets(subscriptions)))
+
+            print("All subscriptions crawled")
+            await asyncio.gather(*tasks)
 
 
 if __name__ == '__main__':
     token = os.getenv('API_TOKEN')
 
-    response = get_refresh_token(token)
+    crawler = Crawler(token)
 
-    if 'access_token' not in response:
-        print(f'Unable to retrieve access token: {response}')
-        exit(1)
-
-    token = response['access_token']
-
-    for subscriptions in get_subscriptions(token):
-        print(json.dumps(subscriptions))
+    asyncio.run(crawler.crawl())
