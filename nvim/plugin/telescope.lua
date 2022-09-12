39cbc7e033c98c3b54e0ddc1d5aff2e170199c36
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-12 18:35:50 +0200

    Fix telescope defaults setup

--- nvim/plugin/telescope.lua
+++ nvim/plugin/telescope.lua
@@ -5,22 +5,21 @@ telescope.setup({
         find_files = {
             hidden = true
         },
-        live_grep = {
-            hidden = true
-        }
     },
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--hidden",
-      "--smart-case"
-    },
-    file_ignore_patterns = {
-        "%.git/*"
+    defaults = {
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--hidden",
+            "--smart-case"
+        },
+        file_ignore_patterns = {
+            "%.git/*"
+        },
     },
 })
 
