b299910fc4b005dfe6a2c1013d9d91e0408769da
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-13 13:37:29 +0200

    More lua goodness

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,33 +1,39 @@
 -- Options
 -- Enables the clipboard between Vim/Neovim and other applications.
-vim.cmd('set clipboard=unnamedplus')
+vim.opt.clipboard = 'unnamedplus'
 -- Modifies the auto-complete menu to behave more like an IDE.
-vim.cmd('set completeopt=noinsert,menuone,noselect')
-vim.cmd('set shortmess+=c')
-vim.cmd('set cursorline') -- Highlights the current line in the editor
-vim.cmd('set hidden') -- Hide unused buffers
-vim.cmd('set autoindent') -- Indent a new line
-vim.cmd('set autoread') -- Read buffers modified outside of Neovim
-vim.cmd('set inccommand=split') -- Show replacements in a split screen
-vim.cmd('set mouse=a') -- Allow to use the mouse in the editor
-vim.cmd('set number') -- Shows the line numbers
-vim.cmd('set splitbelow splitright') -- Change the split screen behavior
-vim.cmd('set title') -- Show file title
-vim.cmd('set wildmenu') -- Show a more advance menu
-vim.cmd('set cc=80') -- Show at 80 column a border for good code style
-vim.cmd('set shiftwidth=4 tabstop=4 expandtab') -- Tabs are now 4 space wide
-vim.cmd('set scrolloff=8')
-vim.cmd('set signcolumn=yes')
-vim.cmd('set relativenumber')
-vim.cmd('set nu')
-vim.cmd('filetype plugin indent on')   -- Allow auto-indenting depending on file type
-vim.cmd('syntax on')
--- vim.cmd(set spell " enable spell check (may need to download language package))
-vim.cmd('set ttyfast') -- Speed up scrolling in Vim
-vim.cmd('set list lcs=tab:==>,trail:·,precedes:<,extends:>,multispace:·,nbsp:○')
-vim.cmd('set updatetime=200')
-vim.cmd('let mapleader = " "')
-vim.cmd('set nohls')
+vim.opt.completeopt = 'noinsert,menuone,noselect'
+vim.opt.shortmess:append('c')
+vim.opt.cursorline = true -- Highlights the current line in the editor
+vim.opt.hidden = true -- Hide unused buffers
+vim.opt.autoindent = true -- Indent a new line
+vim.opt.autoread = true -- Read buffers modified outside of Neovim
+vim.opt.inccommand = 'split' -- Show replacements in a split screen
+vim.opt.mouse = 'a' -- Allow to use the mouse in the editor
+vim.opt.number = true -- Shows the line numbers
+vim.opt.splitbelow = true -- Change the split screen behavior
+vim.opt.title = true -- Show file title
+vim.opt.wildmenu = true -- Show a more advance menu
+vim.opt.cc = '80' -- Show at 80 column a border for good code style
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true -- Tabs are now 4 space wide
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = 'yes'
+vim.opt.relativenumber = true
+vim.opt.ttyfast = true -- Speed up scrolling in Vim
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = '==>',
+    trail = '·',
+    precedes = '<',
+    extends = '>',
+    multispace = '·',
+    nbsp = '○',
+}
+vim.opt.updatetime = 200
+vim.opt.hls = false
+vim.g.mapleader = ' '
 
 local Plug = vim.fn['plug#']
 
@@ -84,8 +90,8 @@ Plug('iamcco/markdown-preview.nvim', { ['do'] = vim.fn['cd app && yarn install']
 
 vim.cmd('call plug#end()')
 
-vim.cmd('set background=dark')
-vim.cmd('set termguicolors')
+vim.opt.background = 'dark'
+vim.opt.termguicolors = true
 vim.cmd('colorscheme nightfly')
 
 local trim_whitespace = function()
@@ -113,3 +119,4 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 vim.api.nvim_set_keymap('n', '<Leader>ej', '<cmd>Ex<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>ev', '<cmd>Vex<CR>', {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>eh', '<cmd>Sex<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>md', '<cmd>MarkdownPreview<CR>', {noremap = true})
