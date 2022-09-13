d79b8fef4e4bcff8a9c341b4fe4f17addcde95a1
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-13 17:09:51 +0200

    Use the proper lua lsp :facepalm:

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -53,7 +53,28 @@ nvim_lsp.clangd.setup({
 nvim_lsp.cmake.setup({})
 
 -- lua setup
-require'lspconfig'.luau_lsp.setup{}
+require'lspconfig'.sumneko_lua.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 
 -- Setup Completion
 -- See https://github.com/hrsh7th/nvim-cmp#basic-configuration
