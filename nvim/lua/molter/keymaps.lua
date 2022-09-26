e1f5a790b9874652e659ed1479725388104a6aaa
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-09-26 12:47:47 +0200

    Add a few more keymaps

--- nvim/lua/molter/keymaps.lua
+++ nvim/lua/molter/keymaps.lua
@@ -1,4 +1,8 @@
 -- Global Keymaps
-vim.keymap.set('n', '<Leader>ej', '<cmd>Ex<CR>', {noremap = true})
-vim.keymap.set('n', '<Leader>ev', '<cmd>Vex<CR>', {noremap = true})
-vim.keymap.set('n', '<Leader>eh', '<cmd>Sex<CR>', {noremap = true})
+vim.keymap.set('n', '<Leader>ej', '<cmd>Ex<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>ev', '<cmd>Vex<CR>', { noremap = true })
+vim.keymap.set('n', '<Leader>eh', '<cmd>Sex<CR>', { noremap = true })
+
+-- Navigate quickfixes
+vim.keymap.set('n', ']q', '<cmd>cn<CR>', { noremap = true })
+vim.keymap.set('n', '[q', '<cmd>cp<CR>', { noremap = true })
