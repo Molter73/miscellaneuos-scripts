d0d1ebf6dd4461508c316f9c9c029a028509b7cf
Mauro Ezequiel Moltrasio <mmoltras@redhat.com>
2022-12-12 18:41:53 +0100

    Add YAML to treesitter + page up and down with auto-center

--- dotfiles/nvim/after/plugin/treesitter.lua
+++ dotfiles/nvim/after/plugin/treesitter.lua
@@ -14,6 +14,7 @@ require('nvim-treesitter.configs').setup({
         'regex',
         'rust',
         'toml',
+        'yaml',
     },
 
     highlight = {
--- dotfiles/nvim/lua/molter/keymaps.lua
+++ dotfiles/nvim/lua/molter/keymaps.lua
@@ -1,7 +1,12 @@
 -- Global Keymaps
+-- netrw
 vim.keymap.set('n', '<Leader>ej', '<cmd>Ex<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>ev', '<cmd>Vex<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>eh', '<cmd>Sex<CR>', { noremap = true })
+
+-- Page up and down with auto-center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
 
 -- Navigate quickfixes
 vim.keymap.set('n', ']q', '<cmd>cn<CR>', { noremap = true })
